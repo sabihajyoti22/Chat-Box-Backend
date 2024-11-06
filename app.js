bf8ed721666c0b5dd4874acb3680bcf9f71f17c7
sabihajyoti22 <sabihajyoti@gmail.com>
2024-11-06 17:47:11 +0600

    chore: changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const { Server } = require("socket.io")
-const http = require("https")
+const http = require("http")
 const cors = require("cors")
 const app = express()
 
