057e738fba2c43a92247304b89fe665b015a3381
sabihajyoti22 <sabihajyoti@gmail.com>
2024-11-06 20:05:00 +0600

    refact: change codes

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const app = require("./app")
 const config = require("./Config/config")
-
+const { Server } = require("socket.io")
 const PORT = config.app.port
 
-app.listen(PORT, () => {
-  console.log(`Server is running at location http://localhost:${PORT}`)
-})
+const io = new Server({ /* options */ });
+
+io.on("connection", (socket) => {
+  console.log(socket.id)
+  socket.on("chatRoom", chatRoom => {
+    socket.join(chatRoom)
+  })
+  
+  socket.on("newMessages", ({newMessages, chatRoom}) => {
+    io.in(chatRoom).emit("getLatestMessage", newMessages)
+  })
+});
+
+io.listen(PORT)
