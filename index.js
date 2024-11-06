5bc122a1bf82b4e323bbeb66ed26cb1bd1f72daa
sabihajyoti22 <sabihajyoti@gmail.com>
2024-11-06 20:06:59 +0600

    refact: back to previous

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 const app = require("./app")
 const config = require("./Config/config")
-const { Server } = require("socket.io")
+
 const PORT = config.app.port
 
-const io = new Server({ /* options */ });
-
-io.on("connection", (socket) => {
-  console.log(socket.id)
-  socket.on("chatRoom", chatRoom => {
-    socket.join(chatRoom)
-  })
-  
-  socket.on("newMessages", ({newMessages, chatRoom}) => {
-    io.in(chatRoom).emit("getLatestMessage", newMessages)
-  })
-});
-
-io.listen(PORT)
+app.listen(PORT, () => {
+  console.log(`Server is running at location http://localhost:${PORT}`)
+})
